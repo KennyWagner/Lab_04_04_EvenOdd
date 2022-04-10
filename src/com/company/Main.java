d1fbe97e8cff05b68ab3e9ed8d38b77ceca963ec
KennyWagner <wagnerkenneth72@gmail.com>
2022-04-10 19:30:13 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+        int numToExamine = 7;
+
+        if (numToExamine % 2 == 0)
+        {
+            System.out.println("Your number is even");
+        }
+        else
+        {
+            System.out.println("Your number is odd");
+        }
+    }
+}
